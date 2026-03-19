44fb63ea1249b98019e02f6f1a3e2ee165a1a150
atulanand-07 <victusatul@gmail.com>
2026-03-20 00:48:56 +0530

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -1,8 +1,14 @@
-from langchain_ollama import ChatOllama
-from config import LLM_MODEL
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from config import LLM_MODEL, TEMPERATURE
+
+load_dotenv()
 
 def get_llm():
-    return ChatOllama(
+    return ChatOpenAI(
         model=LLM_MODEL,
-        temperature=0.3
+        api_key=os.getenv("OPENROUTER_API_KEY"),
+        base_url="https://openrouter.ai/api/v1",
+        temperature=TEMPERATURE
     )
